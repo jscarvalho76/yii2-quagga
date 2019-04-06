13efe64e618ae2c973404cab96eb42f10dd304ea
Jefferson Carvalho <jscarvalho76@gmail.com>
2019-04-06 19:05:23 -0300

    Release 1.0.2

--- assets/js/quagga_modal.js
+++ assets/js/quagga_modal.js
@@ -86,4 +86,17 @@ $(function() {
             Quagga.decodeSingle($.extend({}, fileConfig, {src: URL.createObjectURL(e.target.files[0])}), function(result) {alert(result.codeResult.code);});
         }
     });
+    
+    // Once a barcode had been read successfully, stop quagga and
+    // close the modal after a second to let the user notice where
+    // the barcode had actually been found.
+    Quagga.onDetected(function(result) {
+        if (result.codeResult.code){
+            $('#scanner_input').val(result.codeResult.code);
+            Quagga.stop();
+            if(dataQuagga.submit)
+                $('#leitorEan').submit();
+            setTimeout(function(){ $('#livestream_scanner').modal('hide'); }, 1000);
+        }
+    });
 });

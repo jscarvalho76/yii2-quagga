389db5b29a86ee2ee6a7b2ba17aeb1ae0e05f272
Jefferson Carvalho <jscarvalho76@gmail.com>
2019-04-06 17:10:01 -0300

    Release 1.0.2

--- assets/js/quagga_modal.js
+++ assets/js/quagga_modal.js
@@ -72,17 +72,6 @@ $(function() {
         }
     });
 
-    // Once a barcode had been read successfully, stop quagga and
-    // close the modal after a second to let the user notice where
-    // the barcode had actually been found.
-    Quagga.onDetected(function(result) {
-        if (result.codeResult.code){
-            $('#scanner_input').val(result.codeResult.code);
-            Quagga.stop();
-            setTimeout(function(){ $('#livestream_scanner').modal('hide'); }, 1000);
-        }
-    });
-
     // Stop quagga in any case, when the modal is closed
     $('#livestream_scanner').on('hide.bs.modal', function(){
         if (Quagga){

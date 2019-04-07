a0dddf23775f4dfde8e449103ebbfab2c7deaba0
Jefferson Carvalho <jscarvalho76@gmail.com>
2019-04-07 16:10:35 -0300

    Update _ready.php

--- views/_ready.php
+++ views/_ready.php
@@ -11,19 +11,29 @@ use yii\web\View;
 <?php if($model->submit): ?>
 
     <?php $form = ActiveForm::begin(['id' => $model->id, 'action' => \yii\helpers\Url::toRoute($model->action)]); ?>
+        <div class="input-group">
+            <?= Html::textInput($model->input, '', [
+                'id' => $model->input,
+                'alt' => "Click the button to scan an EAN...",
+                'class' => 'form-control',
+                'placeholder' => "Click the button to scan an EAN..."
+            ]);
+            ?>
 
-    <?= Html::hiddenInput($model->input, '', [
-            'id' => $model->input,
-            'alt' => "Click the button to scan an EAN..."
-    ]);
-    ?>
-
+            <span class="input-group-btn">
+                <button class="btn btn-default" type="button"
+                        <?php if($model->modal): ?>
+                        data-toggle="modal" data-target="#livestream_scanner"
+                        <?php else: ?>
+                        value="Scan" onclick="getScan();"
+                        <?php endif; ?>
+                        title="Click the button to scan an EAN..."  >
+                    <i class="fa fa-barcode"></i>
+                </button>
+            </span>
+        </div>
     <?php ActiveForm::end(); ?>
-    <span class="input-group-btn">
-        <button class="btn btn-default" type="button" data-toggle="modal" data-target="#livestream_scanner" title="Click the button to scan an EAN...">
-            <i class="fa fa-barcode"></i>
-        </button>
-    </span>
+
 
 <?php else: ?>
 
@@ -32,17 +42,25 @@ use yii\web\View;
             <div class="input-group">
 
                 <?= Html::textInput($model->input, '', [
-                'id' => $model->input,
-                'class' => 'form-control',
-                'placeholder' => "Click the button to scan an EAN..."
+                    'id' => $model->input,
+                    'class' => 'form-control',
+                    'placeholder' => "Click the button to scan an EAN..."
                 ]);
                 ?>
 
                 <span class="input-group-btn">
-                    <button class="btn btn-default" type="button" data-toggle="modal" data-target="#livestream_scanner" title="Click the button to scan an EAN...">
+
+                    <button class="btn btn-default" type="button"
+                        <?php if($model->modal): ?>
+                            data-toggle="modal" data-target="#livestream_scanner"
+                        <?php else: ?>
+                            value="Scan" onclick="getScan();"
+                        <?php endif; ?>
+                        title="Click the button to scan an EAN..."  >
                         <i class="fa fa-barcode"></i>
                     </button>
-                </span>
+
+                 </span>
 
             </div><!-- /input-group -->
         </div><!-- /.col-lg-6 -->
@@ -50,22 +68,24 @@ use yii\web\View;
 
 <?php endif; ?>
 
-<div class="modal" id="livestream_scanner">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-                <h4 class="modal-title">Barcode Scanner Ean</h4>
-            </div>
-            <div class="modal-body" style="position: static">
-                <div id="interactive" class="viewport"></div>
-                <div class="error"></div>
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-primary" data-dismiss="modal">Sair</button>
-            </div>
-        </div><!-- /.modal-content -->
-    </div><!-- /.modal-dialog -->
-</div><!-- /.modal -->
+<?php if($model->modal): ?>
+    <div class="modal" id="livestream_scanner">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                    <h4 class="modal-title">Barcode Scanner Ean</h4>
+                </div>
+                <div class="modal-body" style="position: static">
+                    <div id="interactive" class="viewport"></div>
+                    <div class="error"></div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary" data-dismiss="modal">Sair</button>
+                </div>
+            </div><!-- /.modal-content -->
+        </div><!-- /.modal-dialog -->
+    </div><!-- /.modal -->
+<?php endif; ?>

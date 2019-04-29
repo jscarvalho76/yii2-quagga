bc3ee1f8955bf1fb8730c3b4297a04d93a6e8e5f
Jefferson Carvalho <jscarvalho76@gmail.com>
2019-04-29 10:01:41 -0300

    Add translate pt-BR
    Add translate pt-BR

---
+++ messages/pt-BR/quagga.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jefferson.carvalho
+ * Date: 29/04/2019
+ * Time: 09:06
+ */
+return [
+    'Application' => 'Quagga',
+    'Click the button to scan an EAN...' => 'Clique no botão para ler o EAN...',
+];
